f3dff24f6669cd4829dd9bc9441564ba0334fac4
unknown <dumm_heit@xyz.de>
2014-12-03 19:47:07 +0100

    cool4sssssssss

--- coins_class.php
+++ coins_class.php
@@ -35,6 +35,7 @@ class w_coins {
 		$instance_id = 0;
 		$select_instance_id = 0;
 		$tmp = $this->my_all_coins->get_last_w_coins_settings();
+		print_r($tmp);
 		$this->w_coins_settings = $tmp[0];
 		$tmp = $this->my_all_coins->getCoins();
 		$this->coins = $tmp[0];
