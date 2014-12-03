6d4492fa25e67a0fc4d84e72c99402331f80e2e3
unknown <dumm_heit@xyz.de>
2014-12-03 20:56:56 +0100

    cool4ssssssssssssdddddddssdddddddddddddd

--- coins.php
+++ coins.php
@@ -1,4 +1,13 @@
 <?php
 require_once('coins_class.php');
-$my_coins = w_coins::get();
+$my_coins_names="";
+$init_feebee_account="";
+/* 
+//Only needed if you want add your own coins
+$my_coins_names="Bitcoin,Bitcrystal,Bitcrystalx";
+$my_coins_names=explode(",",$my_coins_names);
+$init_feebee_account=false;
+*/
+$my_coins = w_coins::get($my_coins_names,$init_feebee_account);
+
 ?>
